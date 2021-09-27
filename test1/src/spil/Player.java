885926f55222a994cf18ebbccc38dfdee75efe08
Berfin Flora Turan <s215709@student.dtu.dk>
2021-09-27 14:33:19 +0200

    Opstart af projekt

---
+++ test1/src/spil/Player.java
@@ -0,0 +1,13 @@
+package spil;
+
+public class Player {
+    private int point;
+
+    public void setPoint(int point) {
+        this.point = point;
+    }
+
+    public int getPoint() {
+        return point;
+    }
+}
---
+++ test1/src/spil/Spil.java
@@ -0,0 +1,21 @@
+package spil;
+import gui_main.GUI;
+
+public class Spil {
+    private int sum;
+    private boolean ens;
+
+    public int getSum() {
+        return sum;
+    }
+
+    public boolean getEns() {
+        return ens;
+    }
+
+
+    public static void main(String[] args) {
+        GUI gui = new GUI();
+
+    }
+}
