d1905c57272b5dd363ef4b61091a37b0463a9961
Berfin Flora Turan <s215709@student.dtu.dk>
2021-09-27 21:01:54 +0200

    Player og terning objekter. Terning slår, sum bliver fundet og bliver tilføjet til playerens point. Problem: Player 1 og Player 2 får de samme point/terningeslag pga. for i loop

--- test1/src/spil/Player.java
+++ test1/src/spil/Player.java
@@ -2,6 +2,10 @@ package spil;
 
 public class Player {
     private int point;
+    private int terning1;
+    private int terning2;
+
+
 
     public void setPoint(int point) {
         this.point = point;
@@ -9,5 +13,21 @@ public class Player {
 
     public int getPoint() {
         return point;
+    }
+
+    public void setTerning1(int terning1) {
+        this.terning1 = terning1;
+    }
+
+    public void setTerning2(int terning2) {
+        this.terning2 = terning2;
+    }
+
+    public int getTerning2() {
+        return terning2;
+    }
+
+    public int getTerning1() {
+        return terning1;
     }
 }
--- test1/src/spil/Spil.java
+++ test1/src/spil/Spil.java
@@ -2,20 +2,53 @@ package spil;
 import gui_main.GUI;
 
 public class Spil {
-    private int sum;
-    private boolean ens;
-
-    public int getSum() {
-        return sum;
-    }
-
-    public boolean getEns() {
-        return ens;
-    }
 
 
     public static void main(String[] args) {
         GUI gui = new GUI();
+        Terning terning = new Terning();
 
-    }
+        Player player1 = new Player();
+        Player player2 = new Player();
+        int terning1;
+        int terning2;
+        int terning3;
+        int terning4;
+
+
+        player1.setPoint(0);
+        player2.setPoint(0);
+
+
+        for (int runde = 0; player1.getPoint() < 40 && player2.getPoint()<40; runde++) {
+            terning1 = (int) (Math.random() * 6) + 1;
+            terning2 = (int) (Math.random() * 6) + 1;
+
+            gui.setDice(terning1, terning2);
+
+            terning.setSum(terning1+terning2);
+            player1.setPoint(player1.getPoint()+terning.getSum());
+
+            System.out.println("Player 1: terning 1 er: " + terning1 + " og terning 2 er: "+ terning2);
+            System.out.println("Player 1: summen for runden er: " + terning.getSum());
+            System.out.println("Player 1's point er: " + player1.getPoint());
+            System.out.println("-----------------------------------------");
+
+            terning3 = (int) (Math.random() * 6) + 1;
+            terning4 = (int) (Math.random() * 6) + 1;
+            gui.setDice(terning3, terning4);
+
+            terning.setSum(terning3+terning4);
+            player2.setPoint(player1.getPoint()+terning.getSum());
+
+            System.out.println("Player 2: terning 1 er: " + terning3 + " og terning 2 er: "+ terning4);
+            System.out.println("Player 2: summen for runden er: " + terning.getSum());
+            System.out.println("Player 2's point er: " + player1.getPoint());
+            System.out.println("-----------------------------------------");
+        }
+
+
+
+
+}
 }
