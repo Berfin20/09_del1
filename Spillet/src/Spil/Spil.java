aacc0f21a67c608bb52760598fa085dd81bfd97e
Berfin Flora Turan <s215709@student.dtu.dk>
2021-09-29 14:28:27 +0200

    Main metode-kode rykket i en metode runApp() for sig selv og der er tilføjet en velkommen besked

--- Spillet/src/Spil/Spil.java
+++ Spillet/src/Spil/Spil.java
@@ -3,33 +3,31 @@ package Spil;
 import java.util.Scanner;
 
     public class Spil {
+        //Oprettelse af objekter
+        Terning terning1 = new Terning();
+        Terning terning2 = new Terning();
+        Player player1 = new Player();
+        Player player2 = new Player();
 
+        //Sum variabel
+        int sum;
 
-        public static void main(String[] args) {
-            //Oprettelse af objekter
-            Terning terning1 = new Terning();
-            Terning terning2 = new Terning();
-            Player player1 = new Player();
-            Player player2 = new Player();
-
+        void runApp() {
             //Sæt player 1 og player 2's point til nul
             player1.setPoint(0);
             player2.setPoint(0);
-
-            //Sum variabel
-            int sum;
-
-
             //Sætter bestemte taster til hver player med scanneren. Player 1 får bogstavet a og Player 2 får bogstaver l.
             //For hver runde imens player 1 og player 2 har under 40 point så:
+            System.out.println("Player 1 har fået tildelt tasten 'a' & Player 2 har fået tildelt tasten 'l'. Player 1 starter :) Tast 'a'");
             for (int runde = 1; player1.getPoint() < 40 && player2.getPoint() < 40; runde++) {
-                System.out.println("Runde "+ runde);
+                System.out.println("Runde " + runde);
 
                 //Opret scanner opjekt for at kunne scanne spillernes inputs
                 Scanner scanner = new Scanner(System.in);
                 String type = scanner.next();
 
                 //Hvis inputtet er a (uanset stor eller småt bogstav) så
+
                 if (type.equalsIgnoreCase("a")) {
 
                     //Sæt terning objekterne til random værdier mellem 1 og 6
@@ -47,8 +45,12 @@ import java.util.Scanner;
 
                     //Hvis player 1's point eller player 2's point er lig med eller større end 40, så annoncer en vinder og slut programmet
                     if (player1.getPoint() >= 40 || player2.getPoint() >= 40) {
-                        System.out.println("Vinderen er player 1 med " + player1.getPoint()+ " point!");
+                        System.out.println("Vinderen er player 1 med " + player1.getPoint() + " point!");
                         break;
+                    }
+                    if (terning1.getFaceValue() == 1 && terning2.getFaceValue() == 1) {
+                        player1.setPoint(0);
+                        System.out.println("Player 1 har slået to 1'ere og har derfor mistet alle point. Player 1 har: " + player1.getPoint() + " point.");
                     }
 
                     Scanner scanner2 = new Scanner(System.in);
@@ -66,9 +68,26 @@ import java.util.Scanner;
                             System.out.println("Vinderen er player 2 med " + player2.getPoint() + " point!");
                             break;
                         }
+                        if (terning1.getFaceValue() == 1 && terning2.getFaceValue() == 1) {
+                            player2.setPoint(0);
+                            System.out.println("Player 2 har slået to 1'ere og har derfor mistet alle point. Player 1 har: " + player2.getPoint() + " point.");
+                        }
                     }
                 }
             }
         }
-    }
 
+        void welcome(){
+            System.out.println("Velkommen til terninge-spillet. For at starte spillet: tast 's' :)");
+            Scanner scanner = new Scanner(System.in);
+            String tast = scanner.next();
+            if (tast.equalsIgnoreCase("s")) {
+                runApp();
+            }
+        }
+
+        public static void main(String[] args) {
+            Spil spil = new Spil();
+            spil.welcome();
+           }
+        }
