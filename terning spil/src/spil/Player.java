ba0e7c03fefd14be6e2c6ac02197482cf1180f00
Berfin Flora Turan <s215709@student.dtu.dk>
2021-09-29 11:27:15 +0200

    Revert "Færdiggjort spil ud fra minimums krav (ingen ekstra opgaver)"
    This reverts commit 5a6545cead336d35ccf623cdfe8c5a56cb26ad0a.
    
    Revert "fiks småfejl"
    
    This reverts commit 0d8810fbab3f880423ca945338a72082952b5f20.
    
    Revert "fiks småfejl"
    
    This reverts commit 0d8810fbab3f880423ca945338a72082952b5f20.

---
+++ terning spil/src/spil/Player.java
@@ -0,0 +1,33 @@
+package spil;
+
+public class Player {
+    private int point;
+    private int terning1;
+    private int terning2;
+
+
+
+    public void setPoint(int point) {
+        this.point = point;
+    }
+
+    public int getPoint() {
+        return point;
+    }
+
+    public void setTerning1(int terning1) {
+        this.terning1 = terning1;
+    }
+
+    public void setTerning2(int terning2) {
+        this.terning2 = terning2;
+    }
+
+    public int getTerning2() {
+        return terning2;
+    }
+
+    public int getTerning1() {
+        return terning1;
+    }
+}
