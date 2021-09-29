7a15fc12b821f210efab319995bc25513052d24f
Berfin Flora Turan <s215709@student.dtu.dk>
2021-09-29 12:04:37 +0200

    Småændringer

--- Spillet/src/Spil/Player.java
+++ Spillet/src/Spil/Player.java
@@ -2,10 +2,6 @@ package Spil;
 
 public class Player {
     private int point;
-    private int terning1;
-    private int terning2;
-
-
 
     public void setPoint(int point) {
         this.point = point;
@@ -15,19 +11,4 @@ public class Player {
         return point;
     }
 
-    public void setTerning1(int terning1) {
-        this.terning1 = terning1;
-    }
-
-    public void setTerning2(int terning2) {
-        this.terning2 = terning2;
-    }
-
-    public int getTerning2() {
-        return terning2;
-    }
-
-    public int getTerning1() {
-        return terning1;
-    }
 }
--- Spillet/src/Spil/Terning.java
+++ Spillet/src/Spil/Terning.java
@@ -1,26 +1,17 @@
 package Spil;
 
 public class Terning {
-    private int sum;
-    private boolean ens;
     private int faceValue = 0;
 
     public int getFaceValue() {
         return faceValue;
     }
-
     public void setFaceValue(int faceValue) {
         this.faceValue = faceValue;
     }
 
-    public int getSum() {
-        return sum;
-    }
-
-    public void setSum(int sum) {
-        this.sum = sum;
-    }
-    public boolean getEns() {
-        return ens;
+    @Override
+    public String toString() {
+        return String.valueOf(faceValue);
     }
 }
